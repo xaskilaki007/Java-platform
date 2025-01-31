e7d9c784fac9d82e745eb190301eb388a23e2df3
peter <petrspakov49@gmail.com>
2025-01-31 13:44:24 +0300

    здесь появилась возможность добавлять таблицу, удалять поля и вставлять поля таблиц, выводится диаграмма где каждый цвет обозначает количество страниц книги, можно скрыть таблицу и экспортировать Были изменены размеры объектов с помощью scene builder

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("app table.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 1500);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
